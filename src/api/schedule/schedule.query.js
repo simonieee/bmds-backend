6b74c1d616de7d59028f13d8762cd9ece57d94f7
ongdv <35919334+ongdv@users.noreply.github.com>
2021-10-21 14:41:27 +0900

    [UPDATE] Schedule Update
    스케쥴 조회 API 수정
    날짜별 시간 조회로 변경.
    추후에 시간별 예약가능 여부  추가할 예정

--- src/api/schedule/schedule.query.js
+++ src/api/schedule/schedule.query.js
@@ -11,4 +11,26 @@ export default {
     WHERE
         expert_id = :expert_id;`;
   },
+  getDateSchedule() {
+    return `
+    SELECT
+      schedule_id,
+      expert_id,
+      schedule_date
+    FROM
+      t_schedule
+    WHERE
+      expert_id = :expert_id`;
+  },
+  getTimeSchedule() {
+    return `
+    SELECT
+      schedule_time
+    FROM
+      t_schedule
+    WHERE
+      expert_id = :expert_id
+    AND
+      schedule_date = :schedule_date;`;
+  },
 };
